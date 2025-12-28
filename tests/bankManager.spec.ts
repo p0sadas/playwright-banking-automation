2287d96030c0274b89caf18bcb3a32063d6571f9
Angel Posadas <posadasangel9@gmail.com>
2025-12-27 22:10:10 -0600

    second commit

--- tests/bankManager.spec.ts
+++ tests/bankManager.spec.ts
@@ -44,4 +44,15 @@ test.describe("Bank Manager - Data Driven", () => {
       }
     });
   }
+  
+  test("Check if endpoint of reading clients is ok", async ({ request }) => {
+    //since this page has no API I use this as an example but logic is the same
+    const res = await request.get(
+      "https://jsonplaceholder.typicode.com/users/3"
+    );
+    expect(res.ok()).toBeTruthy();
+    const body = await res.json();
+    expect(body.username).toEqual('Samantha');
+    
+  });
 });
